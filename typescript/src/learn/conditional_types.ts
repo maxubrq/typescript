662bd0697cdb2638f3ae6b10efddc3bee821544a
maxubrq <hungtp.play@gmail.com>
2025-11-27 09:17:53 +0700

    :books: docs: add comments

--- typescript/src/learn/conditional_types.ts
+++ typescript/src/learn/conditional_types.ts
@@ -1,4 +1,6 @@
-/// This file is to learn about conditional types in Typescript.
+/// File này là để học về Conditional Types trong Typescript
+///
+/// Conditional Types là việc có thể cấu thành Type từ điều kiện logic
 
 type IdLabel = {
   id: number;
@@ -23,7 +25,8 @@ type NameLabel = {
 //   const conditionalLabel = createLabel(check % 2 === 0 ? 1 : 'string');
 // }
 
-// Using conditional returned type => don't need to create 3 overload functions
+// Sử dụng conditional type để lựa chọn kiểu trả về phù hợp.
+// Nhờ vậy không cần phải dùng đến ba khai báo overload để đạt được hiệu ứng tương tự
 type IdLabelOrNameLabel<T extends number | string> = T extends number
   ? IdLabel
   : NameLabel;
@@ -42,7 +45,9 @@ function example2() {
 
 // Conditional Type Constraints
 
-// We can use conditional to check the constrains.
+// Có thể dùng extends để kiểm tra các quy tắc ràng buộc kết hợp với Conditional Type
+// Ví dụ bên dưới chúng ta sẽ kiểm tra nếu Type (T) có field message bên trong nó sẽ lấy kiểu là kiểu của field "message"
+// Nếu không sẽ trả về kiểu never
 
 type Email = {
   message: string;
@@ -52,7 +57,6 @@ type NoMessageType = {
   something: number;
 };
 
-// First we can use the conditional to both check the constrains and infer the constrains type, in this case, the constrains is the T type must have the message field.
 type MessageOf<T> = T extends { message: unknown } ? T["message"] : never;
 
 type MessageOfEmail = MessageOf<Email>;
@@ -66,7 +70,10 @@ type Num = Flatten<number[]>; // number
 
 // Inferring Within Conditional Types
 
-// If T is a function, return the Return Type of T, otherwise return T itself
+// Có thể dùng từ khóa infer để khai báo (giới thiệu) về một type mới
+// ví dụ bên dưới dùng infer để giới thiệu về type Return là kiểu trả về của hàm số đầu vào
+//
+// Giải thích: GetReturnedType<T> nếu T là một hàm số (không quan tâm đến số lượng tham số của nó) thì sẽ trả về kiểu trả về của nó (Return)
 type GetReturnedType<T> = T extends (...args: never[]) => infer Return
   ? Return
   : T;
@@ -78,6 +85,13 @@ type Num1 = GetReturnedType<() => number>;
 type SameT = GetReturnedType<{ a: number }>;
 
 // Distributive Conditional Types
+//
+// Conditional Types cũng có tính phân phối như những type generic khác
+//
+// ToArrayTypeDis<T> sẽ phân phôi các type nếu T là một union type ví dụ như string | number
+// Thì kết quả sẽ là string[] | number[]
+//
+// Nếu không muốn union bị phân phối mà là gộp (string|number)[] thì dùng [T] để gói tính phân phối lại
 
 type ToArrayTypeDis<T> = T extends any ? T[] : never;
 
